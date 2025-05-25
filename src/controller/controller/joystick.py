36fe8d8df21b8f8cab2b3603c2b5ee1f45d10e74
YunSang123 <fgf7404@naver.com>
2025-05-25 17:17:22 +0900

    Feature - Subscribe proprioception and exteroception from zed-rover-package

--- src/controller/controller/joystick.py
+++ src/controller/controller/joystick.py
@@ -9,6 +9,8 @@ sys.path.insert(0,'/isaac_rover_physical_2.0/src/controller/controller')
 
 from utils.loadpolicy import student_loader
 
+from rover_msgs.msg import Exteroception, Proprioception
+
 #Joystick is the publisher (sends velocities)Motor node is the subscriber.
 
 #When the joystick has 0 lin_vel and 0 ang_vel the robot will turn the wheels to the initial position
@@ -19,13 +21,14 @@ class Physical_osr(Node):
         super().__init__('phy_osr_node')
         self.get_logger().info('Physical_osr node ready!')
         
-        self.subscription = self.create_subscription(Twist, '/joy/joy', self.exteroception_cb,1000)
-        self.subscription = self.create_subscription(Twist, '/joy/joy', self.odometry_cb,1000)
+        self.subscription = self.create_subscription(Exteroception, '/exteroception', self.exteroception_cb,1000)
+        self.subscription = self.create_subscription(Proprioception, '/proprioception', self.proprioception_cb,1000)
     
         # TODO : 변경해야함
         self.publisher_ = self.create_publisher(Twist, '/osr/osr_vel', 1000)
-        timer_period = 0.1  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)   # 5Hz 주기로 실행
+        frequency = 60
+        timer_period = 1/frequency  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)   # 60Hz 주기로 실행
         self.goal_dist = 1
         self.goal_heading = 0.0
         self.sparse = [0.0] * 441
@@ -66,13 +69,13 @@ class Physical_osr(Node):
         self.publisher_.publish(msg)
     
     def exteroception_cb(self, msg):
-        # self.sparse에 값 저장하는 내용
-        # self.dense에 값 저장하는 내용
+        self.sparse = list(msg.sparse)
+        self.dense = list(msg.dense)
         pass
 
-    def odometry_cb(self, msg):
-        # self.goal_dist에 distance 저장하는 내용
-        # self.goal_heading에 heading 저장하는 내용
+    def proprioception_cb(self, msg):
+        self.goal_dist = msg.distance
+        self.goal_heading = msg.heading
         pass
 
 
@@ -82,12 +85,8 @@ def main(args=None):
     phy_osr = Physical_osr()
     rclpy.spin(phy_osr)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     phy_osr.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
